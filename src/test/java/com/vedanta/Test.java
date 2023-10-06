d317515201ff8805ed64a4da65e5d14625d83d0d
kravi <kravi@Keshri>
2023-10-06 17:02:26 +0530

    First commit....

---
+++ src/test/java/com/vedanta/Test.java
@@ -0,0 +1,19 @@
+package com.vedanta;
+
+public class Test {
+
+@org.testng.annotations.Test
+public void heman(){
+		
+	String hungry="yes";
+		
+		if(hungry=="yes") {
+			System.out.println("Eat something");
+		}
+		else 
+		{
+			System.out.println("Do Work");
+		}
+	}
+	
+}
