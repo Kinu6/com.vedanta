cac6fe2319f2e723dae07f82d06acb09429cd879
kravi <kravi@Keshri>
2023-10-09 16:37:08 +0530

    commit 4

---
+++ src/test/java/com/vedanta/Test3.java
@@ -0,0 +1,22 @@
+package com.vedanta;
+
+public class Test3 {
+
+	@org.testng.annotations.Test
+	public void heman(){
+			
+		String hungry="yes";
+			
+			if(hungry =="yes") {
+				System.out.println("Eat something");
+			}
+			else 
+			{
+				System.out.println("Do Work");
+			}
+			System.out.println("LE LE");
+		}
+		
+
+	 
+}
